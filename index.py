8ecda46170e0c36f09d42d52a107567e58b57eec
munilla1 <mucho-muni@hotmail.com>
2022-11-13 13:47:37 +0100

    he creado un index y dentro suya 3 pestañas

---
+++ index.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def principal():
+    return render_template('index.html')
+
+@app.route
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
+if __name__ == "__main__":
+    
+    app.run(host='0.0.0.0',
+            debug=True,
+            port=8080)
